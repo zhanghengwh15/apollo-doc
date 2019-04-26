9c1b38be8ca4a5d080192218a9ed27cbfe725047
zhangheng <506472220@11.com>
2019-04-26 10:07:41 +0800

    原始

--- build/sfc.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  input: 'src',
-  outDir: 'lib',
-  include: '**',
-  constants: {
-    __DOCUTE_VERSION__: JSON.stringify(require('../package').version),
-    __PRISM_VERSION__: JSON.stringify(require('prismjs/package').version)
-  }
-}
